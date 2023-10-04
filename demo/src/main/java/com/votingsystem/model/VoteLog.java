5f71ea9b7087cc301cd02dde2071feccc5a8391d
Alex <68903429+asd54035403@users.noreply.github.com>
2023-10-04 21:20:06 +0800

    Update VoteLog.java

--- demo/src/main/java/com/votingsystem/model/VoteLog.java
+++ demo/src/main/java/com/votingsystem/model/VoteLog.java
@@ -4,20 +4,29 @@ package com.votingsystem.model;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-//@Entity(name = "VoteLog")
-//@Table(name = "VOTE_LOG")
 @Entity
+@Table(name = "VOTE_LOG")
 public class VoteLog {
 
 	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name="VOTER")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
 	private String voter;
-//	@Column(name = "ITEM_NO")
 	private Integer itemNo;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getVoter() {
 		return voter;
