6b4f88562f66b1b8120354515ea3f91bac2f0652
Alex <68903429+asd54035403@users.noreply.github.com>
2023-10-04 17:50:47 +0800

    Update VoteItemRepository.java

--- demo/src/main/java/com/votingsystem/repository/VoteItemRepository.java
+++ demo/src/main/java/com/votingsystem/repository/VoteItemRepository.java
@@ -1,8 +1,10 @@
 package com.votingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.votingsystem.model.VoteItem; 
+import com.votingsystem.model.VoteItem;
+
 @Repository
 public interface VoteItemRepository extends JpaRepository<VoteItem, Integer> {
 }
