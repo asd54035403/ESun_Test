2cd9a85740730fc88229e9ebb7031015deda4b81
Alex <68903429+asd54035403@users.noreply.github.com>
2023-10-04 10:06:31 +0800

    Update VoteLogRepository.java

--- demo/src/main/java/com/votingsystem/repository/VoteLogRepository.java
+++ demo/src/main/java/com/votingsystem/repository/VoteLogRepository.java
@@ -3,6 +3,6 @@ package com.votingsystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.votingsystem.model.VoteLog;
-
+@Repository
 public interface VoteLogRepository extends JpaRepository<VoteLog, Long> {
 }
